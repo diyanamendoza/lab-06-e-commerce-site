7dcab22989f3422f58fff34fe8e2687892807dec
diyanamendoza <diyana.amp@pm.me>
2021-08-23 14:30:08 -0700

    Add product data

---
+++ bookshop/books-data.js
@@ -0,0 +1,48 @@
+export const books = [
+    {
+        id: 001,
+        name: 'The Killing Moon',
+        image: `../assets/${books.id}.jpg`,
+        description: 'Assassin priests, mad kings, and the goddess of death collide in the first book of the Dreamblood Duology by NYT bestselling and three time Hugo-Award winning author N. K. Jemisin.',
+        category: 'N.K. Jemisin',
+        price: 15.63
+
+    },
+    {
+        id: 002,
+        name: 'The Unbroken',
+        image: `../assets/${books.id}.jpg`,
+        description: 'On the far outreaches of a crumbling desert empire, two women--a princess and a soldier--will haggle over the price of a nation in this richly imagined, breath-taking sapphic epic fantasy filled with rebellion, espionage, and assassinations.',
+        category: 'C. L. Clark',
+        price: 15.63
+
+    },
+    {
+        id: 003,
+        name: 'Black Water Sister',
+        image: `../assets/${books.id}.jpg`,
+        description: 'A reluctant medium discovers the ties that bind can unleash a dangerous power in this compelling Malaysian-set contemporary fantasy.',
+        category: 'Zen Cho',
+        price: 15.64
+
+    },
+    {
+        id: 004,
+        name: 'This Poison Heart',
+        image: `../assets/${books.id}.jpg`,
+        description: 'Darkness blooms this new contemporary fantasy about a girl with a unique and deadly power.',
+        category: 'Kalynn Bayron',
+        price: 10.11
+
+    },
+    {
+        id: 005,
+        name: 'The Priory of the Orange Tree',
+        image: `../assets/${books.id}.jpg`,
+        description: 'An epic feminist fantasy of queens, dragonriders, and badass secret wyrm-slaying priestesses.',
+        category: 'Samantha Shannon',
+        price: 18.40
+
+    },
+    
+];
