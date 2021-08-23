d01e32d78895f327f3a6cf88bd96993a914ae5e7
diyanamendoza <diyana.amp@pm.me>
2021-08-23 15:54:17 -0700

    Build product.js

--- bookshop/products.js
+++ bookshop/products.js
@@ -1,1 +1,9 @@
 import { books } from './books-data.js';
+import { renderBooks } from './render-utils.js';
+
+const productsContainer = document.getElementById('products-container');
+
+for (let book of books) {
+    const productDiv = renderBooks(book);
+    productsContainer.append(productDiv);
+}
