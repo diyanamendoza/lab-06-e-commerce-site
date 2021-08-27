e5af55ffb741ef5696a15e4996b82722a7c2affc
diyanamendoza <diyana.amp@pm.me>
2021-08-27 10:45:21 -0700

    Update products.js to use getInventory function instead of original books-data

--- bookshop/products.js
+++ bookshop/products.js
@@ -1,9 +1,11 @@
-import { books } from './books-data.js';
+import { getInventory } from '../utils.js';
+// import { books } from './books-data.js';
 import { renderBooks } from './render-utils.js';
 
 const productsContainer = document.getElementById('products-container');
+const currentInventory = getInventory();
 
-for (let book of books) {
+for (let book of currentInventory) {
     const productDiv = renderBooks(book);
     productsContainer.append(productDiv);
 }
